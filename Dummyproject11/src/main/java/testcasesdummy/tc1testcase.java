28267b79aa67bdc0090bdd6c30f370509422479f
dilip <dilip@LAPTOP-OIM6G9RC>
2023-06-03 17:55:01 +0530

    first dummy by ATE1

---
+++ Dummyproject11/src/main/java/testcasesdummy/tc1testcase.java
@@ -0,0 +1,13 @@
+package testcasesdummy;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc1testcase {
+@Test
+public void m1()
+{
+	Reporter.log("Tc1 passed",true);
+}
+
+}
---
+++ Dummyproject11/src/main/java/testcasesdummy/tc2testcase.java
@@ -0,0 +1,13 @@
+package testcasesdummy;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc2testcase {
+	@Test
+public void m2()
+{
+		Reporter.log("Tc2 passed",true);
+}
+
+}
